3e44d30bb3e38e07a64758f90fc289618ed7219f
rubenmanzanodam <rubenmanzanodam@gmail.com>
2023-09-29 19:34:02 +0200

    subida
    hola mundo en android studio

---
+++ PMDMPR101/app/src/main/java/com/rubenmanzano/pmdm_pr101/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.rubenmanzano.pmdm_pr101
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rubenmanzano.pmdm_pr101.ui.theme.PMDMPR101Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PMDMPR101Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Mundo")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hola $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PMDMPR101Theme {
+        Greeting("Android")
+    }
+}
